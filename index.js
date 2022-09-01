641a81ef5a6cca796b831b98e44723a7e9f4d0b8
VictorMunene <victor.munene@student.moringaschool.com>
2022-09-01 14:23:29 +0300

    Completed

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 let weather = {
-    fetchWeather: function(city){
-        fetch
-        .then((resp) => resp.json())
-        .then((data) => this.displayWeather(data))
-    }
-    
-    displayWeather: function(data){
-        const {name} = data;
-        const {icon} = data.weather[0];
-        const {description} = data.weather[0];
-        const {humidity} = data.main;
-        const {temp} = data.main;
-        const {speed} = data.wind;
-
-        document.querySelector(".weather").innerHTML=`
-        <h2 class = "city">${"Weather in " + name}</h2>
-        <div class = "temp"> ${temp + "degrees Celsius"}</div>
-        <img src = "https://openweathermap.org/img/wn/${icon}.png" alt="" class="icon"/>
-        <div class="description">${description}</div>
-        <div class="humidity">${"Humidity:" + humidity + "%"}</div>
-        <div class="wind">${"Wind Speed:" + speed + "km/h"}</div>
-        `
-        document.body.style.backgroundImage = 
+    apiKey: "40977bd0632dff60a93e022c7f7fc209",
+    fetchWeather: function (city) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=
+        ${city}
+        &units=metric&appid=40977bd0632dff60a93e022c7f7fc209`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
     },
-
-    search: function() {
-        this.fetchWeather(document.querySelector(".search-bar").value)
-    }
-};
-
-document.querySelector(".search-bar").addEventListener("keyup", (event) => {
-    if(event.key === "Enter"){
-        weather.search();
-    }
-})
-
-document.querySelector(".search button").addEventListener("click", () => {
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
-});
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather();
